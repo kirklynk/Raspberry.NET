c6aef192d61e338b754d3f45b48e9b3bcecb7c82
Kirk Kerr <kirklynk@outlook.com>
2025-11-16 22:00:43 -0500

    Readme file and updated config logic

--- RelayPIApp/Readme.md
+++ RelayPIApp/Readme.md
@@ -1,4 +1,4 @@
-﻿### Welcome to Relay PI App
+﻿## Welcome to Relay PI App
 
 This application is designed to interface with the 4 port relay module hardware, providing users with a seamless experience to control and monitor their relay modules.
 
@@ -13,6 +13,18 @@ This is a simple application that allows you to:
 - Web-based control panel
 
 
-## Requirements
-- Raspberry Pi with .NET 10.0 support, ie Pi Zero 2W, Pi 3, Pi 4, Pi 400
+### Requirements
+- .NET 10.0 SDK installed on your development machine
+- Raspberry Pi with .NET 10.0 support, ie Pi Zero 2W, Pi 2, Pi 3, Pi 4, Pi 400
 - Latest Raspbian OS installed
+- 4 Port Relay Module compatible with Raspberry Pi (eg. Inland 4-Channel 5V Relay Module Hat from Microcenter)
+
+### Installation
+1. Clone the repository using Visual Studio 2026 or VS Code.
+2. Open the solution file `RelayPIApp.slnx`.
+3. Build the solution to restore dependencies and compile the application.
+4. Deploy the application to a folder on your development machine,ensuring that target framework is linux-arm64 for 64bit Raspbian. The deployment mode needs to be set to `Self Contained`, this ensure that you can run the application on your Raspberry Pi without any additional installation.
+5. Log in to your Raspberry Pi via SSH or directly using a monitor and keyboard.
+6. Create a directory for the application, e.g., `mkdir ~/home/pi/app`. Do not use Sudo for this step.
+7. Copy the contents of deployed folder to your Raspberry Pi using SCP, SFTP, or a USB drive to this directory. I used this powershell scp command, `scp -r c:\temp pi@relaypi.local:/home/pi/app`. This will be different for you as my application is deployed to `c:\temp` on my development machine and my Pi is named relaypi.local.
+8. Once the contents are copied to the Pi directory. SSH into the PI and change into the directory we copied the contents to on the Pi. This is because we need to make the application executable. Run the command `sudo chmod +x RelayPIApp`.
--- RelayPIApp/Services/HardwareService.cs
+++ RelayPIApp/Services/HardwareService.cs
@@ -1,29 +1,151 @@
-﻿using System.Device.Gpio;
+﻿using Microsoft.Extensions.Options;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Device.Gpio;
+using System.Net;
+using System.Runtime.CompilerServices;
 
 namespace RelayPIApp.Services
 {
-    public class HardwareService
+    public class HardwareService : INotifyPropertyChanged
     {
         private readonly GpioController? _gpioController;
-        private readonly GpioPin? _relay6;
-        private readonly GpioPin? _relay4;
-        private readonly GpioPin? _relay22;
-        private readonly GpioPin? _relay26;
 
-        public bool Relay1 { get => _relay4?.Read() == PinValue.High; set { _relay4?.Write(value ? PinValue.High : PinValue.Low); Console.WriteLine($"Relay 1 clicked"); } }
-        public bool Relay2 { get => _relay6?.Read() == PinValue.High; set { _relay6?.Write(value ? PinValue.High : PinValue.Low); Console.WriteLine($"Relay 2 clicked"); } }
-        public bool Relay3 { get => _relay22?.Read() == PinValue.High; set { _relay22?.Write(value ? PinValue.High : PinValue.Low); Console.WriteLine($"Relay 3 clicked"); } }
-        public bool Relay4 { get => _relay26?.Read() == PinValue.High; set { _relay26?.Write(value ? PinValue.High : PinValue.Low); Console.WriteLine($"Relay 4 clicked"); } }
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public ObservableCollection<Pin> Pins { get; }
 
         public HardwareService()
         {
+            Pins = new ObservableCollection<Pin>
+            {
+                RelayPIApp.Services.Pins.Pin2,
+                RelayPIApp.Services.Pins.Pin3,
+                RelayPIApp.Services.Pins.Pin4,
+                RelayPIApp.Services.Pins.Pin5,
+                RelayPIApp.Services.Pins.Pin6,
+                RelayPIApp.Services.Pins.Pin7,
+                RelayPIApp.Services.Pins.Pin8,
+                RelayPIApp.Services.Pins.Pin9,
+                RelayPIApp.Services.Pins.Pin10,
+                RelayPIApp.Services.Pins.Pin11,
+                RelayPIApp.Services.Pins.Pin12,
+                RelayPIApp.Services.Pins.Pin13,
+                RelayPIApp.Services.Pins.Pin14,
+                RelayPIApp.Services.Pins.Pin15,
+                RelayPIApp.Services.Pins.Pin16,
+                RelayPIApp.Services.Pins.Pin17,
+                RelayPIApp.Services.Pins.Pin18,
+                RelayPIApp.Services.Pins.Pin19,
+                RelayPIApp.Services.Pins.Pin20,
+                RelayPIApp.Services.Pins.Pin21,
+                RelayPIApp.Services.Pins.Pin22,
+                RelayPIApp.Services.Pins.Pin23,
+                RelayPIApp.Services.Pins.Pin24,
+                RelayPIApp.Services.Pins.Pin25,
+                RelayPIApp.Services.Pins.Pin26,
+                RelayPIApp.Services.Pins.Pin27
+            };
+            foreach (var pin in Pins)
+            {
+                pin.PropertyChanged += (sender, args) => OnPropertyChanged(nameof(pin));
+            }
+
 #if !DEBUG
-                _gpioController = new GpioController();
-                _relay6 = _gpioController.OpenPin(6);
-                _relay4 = _gpioController.OpenPin(4);
-                _relay22 = _gpioController.OpenPin(22);
-                _relay26 = _gpioController.OpenPin(26);
+            _gpioController = new GpioController();
+           
+            Console.WriteLine("HardwareService: GPIO pins initialized.");
 #endif
+            var pin4 = Pins.First(p => p.Number == 4);
+            pin4.Name = "Relay 1";
+            UpdatePinUsage(pin4, true); // Example: Set pin 4 as not in use initially
+            var pin6 = Pins.First(p => p.Number == 6);
+            pin6.Name = "Relay 2";
+            UpdatePinUsage(pin6, true); // Example: Set pin 6 as not in use initially
+            var pin22 = Pins.First(p => p.Number == 22);
+            pin22.Name = "Relay 3";
+            UpdatePinUsage(pin22, true); // Example: Set pin 22 as not in use initially
+            var pin26 = Pins.First(p => p.Number == 26);
+            pin26.Name = "Relay 4";
+            UpdatePinUsage(pin26, true); // Example: Set pin 26 as not in use initially
         }
+
+        public void UpdatePinUsage(Pin pin, bool inUse)
+        {
+            if (inUse)
+            {
+                pin.GpioPin = _gpioController?.OpenPin(pin.Number, pin.Mode == PinMode.Output ? PinMode.Output : PinMode.Input);
+                pin.InUse = true;
+
+            }
+            else
+            {
+                if (pin.GpioPin != null)
+                {
+                    pin.GpioPin.Write(PinValue.Low);
+                    pin.GpioPin.Close();
+                    pin.GpioPin.Dispose();
+                    pin.GpioPin = null;
+                }
+                pin.InUse = false;
+            }
+            OnPropertyChanged(nameof(pin));
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+
+    public record Pin() : INotifyPropertyChanged
+    {
+        public int Order { get; internal set; } = 0;
+        public int Number { get; init; }
+        public PinMode Mode { get; init; } = PinMode.Output;
+        public bool InUse { get; set; }
+
+        public string? Name { get => field; set { field = value; OnPropertyChanged(); } }
+        public bool State { get => GpioPin?.Read() == PinValue.High; set { GpioPin?.Write(value ? PinValue.High : PinValue.Low); OnPropertyChanged(); } }
+
+        internal GpioPin? GpioPin { get; set; } = null;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class Pins
+    {
+        public static readonly Pin Pin2 = new() { Order = 0, Number = 2, Mode = PinMode.Output, Name = "Gpio 2" };
+        public static readonly Pin Pin3 = new() { Order = 1, Number = 3, Mode = PinMode.Output, Name = "Gpio 3" };
+        public static readonly Pin Pin4 = new() {Order = 2, Number = 4, Mode = PinMode.Output, Name = "Gpio 4" };
+        public static readonly Pin Pin5 = new() {Order = 3, Number = 5, Mode = PinMode.Output, Name = "Gpio 5" };
+        public static readonly Pin Pin6 = new() {Order = 4, Number = 6, Mode = PinMode.Output, Name = "Gpio 6" };
+        public static readonly Pin Pin7 = new() {Order = 5, Number = 7, Mode = PinMode.Output, Name = "Gpio 7" };
+        public static readonly Pin Pin8 = new() {Order = 6, Number = 8, Mode = PinMode.Output, Name = "Gpio 8" };
+        public static readonly Pin Pin9 = new() {Order = 7, Number = 9, Mode = PinMode.Output, Name = "Gpio 9" };
+        public static readonly Pin Pin10 = new() {Order= 8, Number = 10, Mode = PinMode.Output, Name = "Gpio 10" };
+        public static readonly Pin Pin11 = new() {Order= 9, Number = 11, Mode = PinMode.Output, Name = "Gpio 11" };
+        public static readonly Pin Pin12 = new() {Order= 10, Number = 12, Mode = PinMode.Output, Name = "Gpio 12" };
+        public static readonly Pin Pin13 = new() {Order= 11, Number = 13, Mode = PinMode.Output, Name = "Gpio 13" };
+        public static readonly Pin Pin14 = new() {Order= 12, Number = 14, Mode = PinMode.Output, Name = "Gpio 14" };
+        public static readonly Pin Pin15 = new() {Order= 13, Number = 15, Mode = PinMode.Output, Name = "Gpio 15" };
+        public static readonly Pin Pin16 = new() {Order= 14, Number = 16, Mode = PinMode.Output, Name = "Gpio 16" };
+        public static readonly Pin Pin17 = new() {Order= 15, Number = 17, Mode = PinMode.Output, Name = "Gpio 17" };
+        public static readonly Pin Pin18 = new() {Order= 16, Number = 18, Mode = PinMode.Output, Name = "Gpio 18" };
+        public static readonly Pin Pin19 = new() {Order= 17, Number = 19, Mode = PinMode.Output, Name = "Gpio 19" };
+        public static readonly Pin Pin20 = new() {Order= 18, Number = 20, Mode = PinMode.Output, Name = "Gpio 20" };
+        public static readonly Pin Pin21 = new() {Order= 19, Number = 21, Mode = PinMode.Output, Name = "Gpio 21" };
+        public static readonly Pin Pin22 = new() {Order= 20, Number = 22, Mode = PinMode.Output, Name = "Gpio 22" };
+        public static readonly Pin Pin23 = new() {Order= 21, Number = 23, Mode = PinMode.Output, Name = "Gpio 23" };
+        public static readonly Pin Pin24 = new() {Order= 22, Number = 24, Mode = PinMode.Output, Name = "Gpio 24" };
+        public static readonly Pin Pin25 = new() {Order= 23, Number = 25, Mode = PinMode.Output, Name = "Gpio 25" };
+        public static readonly Pin Pin26 = new() {Order= 24, Number = 26, Mode = PinMode.Output, Name = "Gpio 26" };
+        public static readonly Pin Pin27 = new() {Order= 25, Number = 27, Mode = PinMode.Output, Name = "Gpio 27" };
     }
 }
